878760744da78cd0168c27cf93b4f5a3878da96d
hanuman <a.kolyan@gratun.am>
2021-04-12 01:56:00 +0400

    d3. sankey partially integrated

--- script.js
+++ script.js
@@ -1,11 +1,24 @@
+const dataURL = "data/data.json"
+const nodeWidth = 20 
+const nodePadding = 10
+let data = {}
+const filterObject = {
+    source:"",
+    target:""
+} 
+
+
 $(document).ready(function () {
-    const dataURL = "data.json"
     $.get(dataURL, initSankeyDiagram, "json");
 
-
-    function initSankeyDiagram(data, textStatus, jqXHR) {
+    function initSankeyDiagram(_data) {
+        
+        data = _data
+        checkData(data)
         initSourceSelect(data)
         initDestinationsSelect(data)
+
+        showSankeyD3(data, "#sankey", { nodeWidth, nodePadding })
 
         /* //just tests 
         const filtered = filterBy(data, "appg", "Beer")
@@ -17,14 +30,13 @@ $(document).ready(function () {
 
     function initSourceSelect(data) {
         const companies = getSuggestionList(data, "source")
-        initTypeHead('#search_sources',onOptionSelected,{data:companies, title:"Company"})
+        initTypeHead('#search_sources', onOptionSelected.bind(data), { data: companies, title: "Company" })
     }
 
     function initDestinationsSelect(data) {
         const appgs = getSuggestionList(data, "appg")
         const mps = getMPsList(data)
-        initTypeHead('#search_destinations',onOptionSelected, {data:appgs, title:"APPG"},{data:mps, title:"MP"})
+        initTypeHead('#search_destinations', onOptionSelected, { data: appgs, title: "APPG" }, { data: mps, title: "MP" })
     }
-
 
 });
