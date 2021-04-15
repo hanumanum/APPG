4d1510b2ec24eae2f08404cf891acffadbbd9d11
hanuman <a.kolyan@gratun.am>
2021-04-15 18:35:51 +0400

    new filters and clean up

--- script.js
+++ script.js
@@ -1,42 +1,28 @@
-const dataURL = "data/data.json"
+const dataURL = "/data/data.json"
 const nodeWidth = 20 
 const nodePadding = 10
 let data = {}
 const filterObject = {
     source:undefined,
     target:undefined
-} 
-
+}
 
 $(document).ready(function () {
     $.get(dataURL, initSankeyDiagram, "json");
 
     function initSankeyDiagram(_data) {
-        
-        data = _data
-        checkData(data)
-        initSourceSelect(data)
-        initDestinationsSelect(data)
-
-        showSankeyD3(data, "#sankey", { nodeWidth, nodePadding })
-
-        /* //just tests 
-        const filtered = filterBy(data, "appg", "Beer")
-        const filteredByMP = filterByMP(data, "Dehenna Davison")
-        console.log({filtered}, {filteredByMP})
-        //end of just tests 
-        */
-    }
-
-    function initSourceSelect(data) {
-        const companies = getSuggestionList(data, "source")
-        initTypeHead('#search_sources', onOptionSelected.bind(data), { data: companies, title: "Company" })
+        checkData(_data)
+        initDestinationsSelect(_data)
+        showSankeyD3(_data, "#sankey", { nodeWidth, nodePadding })
     }
 
     function initDestinationsSelect(data) {
         const appgs = getSuggestionList(data, "appg")
         const mps = getMPsList(data)
-        initTypeHead('#search_destinations', onOptionSelected, { data: appgs, title: "APPG" }, { data: mps, title: "MP" })
+        const sources = getSuggestionList(data, "source")
+        const years = getSuggestionList(data,"date")
+        initTypeHeadV2('#search_destinations', onOptionSelected, { data: appgs, title: "APPG" }, { data: mps, title: "MP" }, { data:sources, title:"Sources"})
+        initTypeHead('#search_years', onYearSelected, { data: years, title: "Year" })
     }
 
 });
