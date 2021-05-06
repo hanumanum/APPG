86c1f4482a70d84b7b864aa407129f4dd9fe3b2e
hanuman <a.kolyan@gratun.am>
2021-05-06 15:02:44 +0400

    little preparations before implementing table version

---
+++ table/src/js/script.js
@@ -0,0 +1,36 @@
+const dataURL = "../data/data_all.json"
+const nodeWidth = 20 
+const nodePadding = 25
+const DEBUG = true
+initMouseCoordinates()
+let datarepo;
+
+
+$(document).ready(function () {
+    $.get(dataURL, initSankeyDiagram, "json");
+
+    function initSankeyDiagram(_data) {
+        data = _data
+        
+        datarepo = dataRepository(data)
+        initTypeheadSelect(datarepo.getAll())
+        //const bounds = datarepo.getValueBounds()
+        //console.log(bounds)
+
+        datarepo.addFilter("top", true)
+        datarepo.addFilter("year", 2020)
+        const top = datarepo.getFiltered()
+        showSankeyD3(top, "#sankey", { nodeWidth, nodePadding })
+    }
+
+    function initTypeheadSelect(data) {
+        const appgs = datarepo.getSuggestionList("appg")
+        const mps = datarepo.getMPsList()
+        const sources = datarepo.getSuggestionList("source")
+        const years = datarepo.getSuggestionList("date")
+        years.push("All years")
+        initTypeHead('#search_destinations', onOptionSelected, [{ data: mps, title: "MP" }, { data: appgs, title: "APPG" },  { data:sources, title:"Sources"}])
+        initTypeHead('#search_years', onYearSelected, [{ data: years, title: "Year" }])
+    }
+
+});
